4bc94b6958222a21646d8bbb3a2c00c97e4724bd
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-12 16:37:46 +0700

    d

--- test/as/transfer.ts
+++ test/as/transfer.ts
@@ -1,4 +1,14 @@
+import { Cursor } from "@hazae41/cursor";
+
 declare global {
+  interface Uint8Array {
+    toHex(): string;
+  }
+
+  interface Uint8ArrayConstructor {
+    fromHex(hex: string): Uint8Array<ArrayBuffer>;
+  }
+
   interface Uint8Array {
     toBase64(): string;
   }
@@ -8,10 +18,106 @@ declare global {
   }
 }
 
-const verifierAsBytes = Uint8Array.fromBase64("QByZynvXGhaEscyTs8L2h8FWBnNIpAq52mE8SkeLSvQ=")
-const signerAsBytes = Uint8Array.fromBase64("MC4CAQAwBQYDK2VwBCIEIOZmpSIQYsiOya6stoqWQ2cOBcuN0F/AmmU2c0wldqXb")
+type Pack = Array<number | bigint | Uint8Array | Pack | null>
 
-const verifier = await crypto.subtle.importKey("raw", verifierAsBytes, "Ed25519", true, ["verify"]);
-const signer = await crypto.subtle.importKey("pkcs8", signerAsBytes, "Ed25519", true, ["sign"]);
+const size = (input: Pack): number => {
+  let length = 0
 
-export { };
+  for (const arg of input) {
+    if (typeof arg === "number") {
+      length += 1 + 4
+      continue
+    }
+
+    if (typeof arg === "bigint") {
+      length += 1 + 8
+      continue
+    }
+
+    if (arg instanceof Uint8Array) {
+      length += 1 + 4 + arg.length
+      continue
+    }
+
+    if (Array.isArray(arg)) {
+      length += 1 + 4 + size(arg)
+      continue
+    }
+
+    length += 1
+    continue
+  }
+
+  return length
+}
+
+const encode = (input: Pack): Uint8Array<ArrayBuffer> => {
+  const bytes = new Uint8Array(size(input))
+
+  const cursor = new Cursor(bytes)
+
+  for (const arg of input) {
+    if (typeof arg === "number") {
+      cursor.writeUint8OrThrow(1)
+      cursor.writeUint32OrThrow(arg, true)
+      continue
+    }
+
+    if (typeof arg === "bigint") {
+      cursor.writeUint8OrThrow(2)
+      cursor.writeUint64OrThrow(arg, true)
+      continue
+    }
+
+    if (arg instanceof Uint8Array) {
+      cursor.writeUint8OrThrow(3)
+      cursor.writeUint32OrThrow(arg.length, true)
+      cursor.writeOrThrow(arg)
+      continue
+    }
+
+    if (Array.isArray(arg)) {
+      cursor.writeUint8OrThrow(4)
+      const packed = encode(arg)
+      cursor.writeUint32OrThrow(packed.length, true)
+      cursor.writeOrThrow(packed)
+      continue
+    }
+
+    cursor.writeUint8OrThrow(0)
+  }
+
+  return bytes
+}
+
+const signer = await crypto.subtle.importKey("pkcs8", Uint8Array.fromBase64("MC4CAQAwBQYDK2VwBCIEIOZmpSIQYsiOya6stoqWQ2cOBcuN0F/AmmU2c0wldqXb"), "Ed25519", true, ["sign"]);
+const verifier = Uint8Array.fromBase64("QByZynvXGhaEscyTs8L2h8FWBnNIpAq52mE8SkeLSvQ=")
+
+const submodule = Uint8Array.fromHex("1c456b66ee0002cb3339e07ebbfe857a625241e25cd604436ce6b39072608d4f")
+const submethod = new TextEncoder().encode("transfer")
+const subargs = encode([Uint8Array.fromHex("deadbeef"), 42])
+
+const message = encode([submodule, submethod, subargs, 0n])
+const signature = new Uint8Array(await crypto.subtle.sign("Ed25519", signer, message))
+
+const args = encode([submodule, submethod, subargs, verifier, signature])
+
+const body = new FormData()
+body.append("name", "74543f8c148d9efb3ef5ffd5daaeffec74c58ab0506f230451c8fd4689bac3f1")
+body.append("func", "main")
+body.append("args", new Blob([args]))
+
+{
+  const start = performance.now()
+
+  const response = await fetch("http://bob.localhost:8080/api/execute", { method: "POST", body });
+
+  if (!response.ok)
+    throw new Error("Failed", { cause: response })
+
+  console.log(await response.bytes().then(r => r.toHex()))
+
+  const until = performance.now()
+
+  console.log(`Executed in ${(until - start).toFixed(2)}ms`)
+}
