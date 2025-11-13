56e78a38cdc13955655b461312d6422ff54db7d0
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-13 17:56:22 +0700

    d

--- test/as/src/libs/console/mod.ts
+++ test/as/src/libs/console/mod.ts
@@ -4,6 +4,10 @@ export namespace console {
 
   // @ts-ignore: decorator
   @external("console", "log")
-  export declare function log(message: blobs.blob): void
+  export declare function logAsBlob(message: blobs.blob): void
+
+  export function logAsString(message: string): void {
+    logAsBlob(blobs.save(String.UTF8.encode(message)))
+  }
 
 }
