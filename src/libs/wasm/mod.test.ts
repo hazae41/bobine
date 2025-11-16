ede9177b092d4bedd462c9f9f7cbd87f4ff8d2ff
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-16 12:32:30 +0700

    d

--- src/libs/wasm/mod.test.ts
+++ src/libs/wasm/mod.test.ts
@@ -1,10 +1,12 @@
-import { Readable, Writable } from "@hazae41/binary";
+import { Readable } from "@hazae41/binary";
 import { Cursor } from "@hazae41/cursor";
 import { readFileSync } from "node:fs";
 import process from "node:process";
-import { LEB128, Module, Section } from "./mod.ts";
+import { Module, Section } from "./mod.ts";
 
 for (const path of process.argv.slice(2)) {
+  const start = performance.now()
+
   const input = readFileSync(path)
 
   const module = Module.readOrThrow(new Cursor(input))
@@ -12,18 +14,16 @@ for (const path of process.argv.slice(2)) {
   for (const section of module.body.sections) {
     if (section.type !== Section.CodeSection.type)
       continue
-    const code = Readable.readFromBytesOrThrow(Section.CodeSection, section.data)
-
-    for (const func of code.functions) {
-      for (const instruction of func.instructions) {
-        console.log(instruction)
-      }
-    }
+    Readable.readFromBytesOrThrow(Section.CodeSection, section.data)
   }
+
+  const until = performance.now()
+
+  console.log(`Parsed ${path} in ${(until - start).toFixed(2)}ms`)
 }
 
-const u128 = Writable.writeToBytesOrThrow(new LEB128.U64(128n))
-console.log(u128, Readable.readFromBytesOrThrow(LEB128.U64, u128))
+// const u128 = Writable.writeToBytesOrThrow(new LEB128.U64(128n))
+// console.log(u128, Readable.readFromBytesOrThrow(LEB128.U64, u128))
 
-const in1 = Writable.writeToBytesOrThrow(new LEB128.I64(-1n))
-console.log(in1, Readable.readFromBytesOrThrow(LEB128.I64, in1))
+// const in1 = Writable.writeToBytesOrThrow(new LEB128.I64(-1n))
+// console.log(in1, Readable.readFromBytesOrThrow(LEB128.I64, in1))
