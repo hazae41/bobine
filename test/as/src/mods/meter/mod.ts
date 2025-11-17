5990ea95b0e639e4e88f1270b9c7036bfacebaef
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-18 00:00:36 +0700

    x

--- test/as/src/mods/meter/mod.ts
+++ test/as/src/mods/meter/mod.ts
@@ -1,8 +1,11 @@
 import { blobs } from "../../libs/blobs/mod";
-import { console } from "../../libs/console/mod";
+import { sha256 } from "../../libs/sha256/mod";
 
-export function main(): void {
-  for (let i = 0; i < 10; i++) {
-    console.log(blobs.save(String.UTF8.encode("Hello world!")))
-  }
+export function main(): externref {
+  let result = sha256.digest(blobs.save(String.UTF8.encode("Hello world!")))
+
+  for (let i = 0; i < 10; i++)
+    result = sha256.digest(result)
+
+  return result
 }
