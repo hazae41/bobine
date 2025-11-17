3c0b959d9a5d99122e9d2b46ee4dd5bfc40b1757
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-17 16:51:29 +0700

    d

--- test/as/src/mods/meter/mod.ts
+++ test/as/src/mods/meter/mod.ts
@@ -1,11 +1,8 @@
 import { blobs } from "../../libs/blobs/mod";
 import { console } from "../../libs/console/mod";
 
-// @ts-ignore
-@external("metering", "meter")
-declare function meter(value: u64): void
-
 export function main(): void {
-  console.log(blobs.save(String.UTF8.encode("Hello, world!")))
-  meter(100)
+  for (let i = 0; i < 10; i++) {
+    console.log(blobs.save(String.UTF8.encode("Hello world!")))
+  }
 }
