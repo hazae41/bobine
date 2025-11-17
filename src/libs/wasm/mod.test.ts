56717a8065486c4d1a0b9073d2c50f969d2a3e9c
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-11-17 21:49:08 +0700

    d

--- src/libs/wasm/mod.test.ts
+++ src/libs/wasm/mod.test.ts
@@ -1,9 +1,9 @@
-import { Readable, Writable } from "@hazae41/binary";
+import { Writable } from "@hazae41/binary";
 import { Cursor } from "@hazae41/cursor";
 import { Buffer } from "node:buffer";
 import { readFileSync } from "node:fs";
 import process from "node:process";
-import { LEB128, Module } from "./mod.ts";
+import { Module } from "./mod.ts";
 
 for (const path of process.argv.slice(2)) {
   const start = performance.now()
@@ -14,16 +14,12 @@ for (const path of process.argv.slice(2)) {
 
   const output = Writable.writeToBytesOrThrow(module)
 
-  const remodule = Module.readOrThrow(new Cursor(output))
+  Module.readOrThrow(new Cursor(output))
 
-  console.log(Buffer.compare(Buffer.from(input), Buffer.from(output)) === 0 ? "✅" : "❌")
-
-  // writeFileSync("./a.txt", input.toHex())
-  // writeFileSync("./b.wasm", output)
+  if (Buffer.compare(Buffer.from(input), Buffer.from(output)) !== 0)
+    console.log(`Mismatch for ${path}`)
 
   const until = performance.now()
 
   console.log(`Parsed ${path} in ${(until - start).toFixed(2)}ms`)
 }
-
-console.log(Readable.readFromBytesOrThrow(LEB128.I64, Writable.writeToBytesOrThrow(new LEB128.I64(-8446744073709551616n))))
