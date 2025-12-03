b28bb3da2746fcc42c50d39529f5896f1224bbc1
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-12-03 08:50:36 +0100

    d

--- test/run/mods/ed25519.ts
+++ test/run/mods/ed25519.ts
@@ -13,7 +13,7 @@ async function execute(module: string, method: string, params: Uint8Array<ArrayB
 
   const start = performance.now()
 
-  const response = await fetch("http://localhost:8080/api/execute", { method: "POST", body });
+  const response = await fetch("https://bob0.deno0.hazae41.me/api/execute", { method: "POST", body });
 
   const until = performance.now()
 
